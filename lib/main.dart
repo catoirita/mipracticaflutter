4254e49a3f3623d6ec043d51dcaa7844c0ce3ad4
gatoirita <clari.catoira@gmail.com>
2025-06-03 20:36:20 -0400

    formulario base completado con cabecera basica

--- lib/main.dart
+++ lib/main.dart
@@ -20,12 +20,62 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: RegistrationFormScreen(),
+      home: const RegistrationFormScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
-class RegistrationFormScreen extends StatelessWidget{
+
+class RegistrationFormScreen extends StatelessWidget {
+  const RegistrationFormScreen({super.key});
+
   static const Color primaryNavy = Color(0xFF1E3A8A);
-  static const Color backgroundGray = Color (0xFFF9FAFB);
+  static const Color backgroundGray = Color(0xFFF9FAFB);
+  static const Color secondaryGray = Colors.grey;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundGray,
+      appBar: AppBar(
+        title: const Text('Registro'),
+        backgroundColor: primaryNavy,
+      ),
+      body: Center(
+        child: Container(
+            padding: const EdgeInsets.all(24),
+            decoration: BoxDecoration(
+              color: Colors.white, // Color de fondo del Container
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                  color: secondaryGray.withOpacity(0.1),
+                  blurRadius: 10,
+                  offset: const Offset(0, 4),
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                Icon(Icons.person_add, size: 48, color: primaryNavy),
+                SizedBox(height: 12),
+                Text(
+                  "Datos personales",
+                  style:  TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: primaryNavy,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  "Complete todos los campos requeridos",
+                  style: TextStyle(fontSize: 14,color: secondaryGray),
+                )
+              ],
+            )
+        ),
+      ),
+    );
+  }
 }
