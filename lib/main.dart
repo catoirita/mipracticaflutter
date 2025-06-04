67649d7c1141a3d1f7620fb75fe4d7124b548e4b
gatoirita <clari.catoira@gmail.com>
2025-06-03 20:08:07 -0400

    SUBIDA DE TRABAJO CON ERROR

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-return MaterialApp(
- title: "Formulario de registro"
-);
+    return MaterialApp(
+      title: "Formulario de registro",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF1E3A8A),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF1E3A8A),
+          secondary: const Color(0xFF6B7280),
+        ),
+        useMaterial3: true,
+      ),
+      home: RegistrationFormScreen(),
+      debugShowCheckedModeBanner: false,
+    );
   }
-
+}
+class RegistrationFormScreen extends StatelessWidget{
+  static const Color primaryNavy = Color(0xFF1E3A8A);
+  static const Color backgroundGray = Color (0xFFF9FAFB);
 }
